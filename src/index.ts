6f55c53134d5bdf6eef07ddcc9150e66e190e92c
RICARDO AZZI SILVA <ricardo.azzi-ext@viavarejo.com.br>
2020-03-17 19:53:29 -0300

    Exportando da forma correta... ops

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-export * from '.';
-
+export * from './adapter-impl.type';
+export * from './common-error.adapter';
+export * from './custom-error-adapter.interface';
+export * from './error-normalized.interface';
+export * from './error.converter';
